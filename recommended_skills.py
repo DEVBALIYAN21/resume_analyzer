b01e86e5c296f35ded342192124819a3b00d26d1
Ashray V B <161327518+AshrayVB30@users.noreply.github.com>
2024-06-26 21:27:13 +0530

    Update recommended_skills.py

--- recommended_skills.py
+++ recommended_skills.py
@@ -3,3 +3,22 @@ web_skills = ['React', 'Django', 'Node JS', 'React JS', 'php', 'laravel', 'Magen
 android_skills = ['Android', 'Android development', 'Flutter', 'Kotlin', 'XML', 'Java', 'Kivy', 'GIT', 'SDK', 'SQLite']
 ios_skills = ['IOS', 'IOS Development', 'Swift', 'Cocoa', 'Cocoa Touch', 'Xcode', 'Objective-C', 'SQLite', 'Plist', 'StoreKit', "UI-Kit", 'AV Foundation', 'Auto-Layout']
 uiux_skills = ['UI', 'User Experience', 'Adobe XD', 'Figma', 'Zeplin', 'Balsamiq', 'Prototyping', 'Wireframes', 'Storyframes', 'Adobe Photoshop', 'Editing', 'Illustrator', 'After Effects', 'Premier Pro', 'Indesign', 'Wireframe', 'Solid', 'Grasp', 'User Research']
+
+
+# Data Science skills
+ds_skills = ['Data Visualization', 'Predictive Analysis', 'Statistical Modeling', 'Machine Learning Algorithms', 'TensorFlow']
+
+# Web development skills
+web_skills = ['HTML','CSS','JavaScript','React', 'Django', 'Node.js', 'PHP', 'JavaScript']
+ 
+# Android devlopment skills
+android_skills = ['Android', 'Kotlin', 'Java', 'Flutter', 'XML']
+
+# IOS development skils
+ios_skills = ['iOS Development', 'Swift', 'Objective-C', 'Xcode', 'Cocoa Touch']
+
+# UI/UX devlopment skills
+uiux_skills = ['UI', 'User Experience', 'Adobe XD', 'Figma', 'Prototyping']
+
+# Soft Skills 
+soft_skill = ['Communication', 'Teamwork', 'Problem-Solving', 'Time Management', 'Adaptability', 'Critical Thinking', 'Creativity', 'Leadership', 'Attention to Detail', 'Conflict Resolution']
