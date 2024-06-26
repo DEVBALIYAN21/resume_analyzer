ec569e97b0877aefce97d637c248bd99c7e966d7
Ashray V B <161327518+AshrayVB30@users.noreply.github.com>
2024-06-26 21:48:05 +0530

    Update recommended_skills.py

--- recommended_skills.py
+++ recommended_skills.py
@@ -6,19 +6,30 @@ uiux_skills = ['UI', 'User Experience', 'Adobe XD', 'Figma', 'Zeplin', 'Balsamiq
 
 
 # Data Science skills
-ds_skills = ['Data Visualization', 'Predictive Analysis', 'Statistical Modeling', 'Machine Learning Algorithms', 'TensorFlow']
+ds_skills = ['Data Visualization', 'Predictive Analysis', 'Statistical Modeling', 'Machine Learning Algorithms', 'TensorFlow',
+             'Data Mining', 'Clustering & Classification', 'Data Analytics', 'Quantitative Analysis', 'Web Scraping',
+             'Keras', 'PyTorch', 'Probability', 'Scikit-learn', 'Flask']
 
 # Web development skills
-web_skills = ['HTML','CSS','JavaScript','React', 'Django', 'Node.js', 'PHP', 'JavaScript']
+web_skills = ['HTML', 'CSS', 'JavaScript','React', 'Django', 'Node.js', 'PHP', 'JavaScript',
+              'React.js', 'Laravel', 'Magento', 'WordPress', 'Angular.js',
+              'C#', 'Flask', 'SDK', 'HTML/CSS', 'RESTful APIs']
  
 # Android devlopment skills
-android_skills = ['Android', 'Kotlin', 'Java', 'Flutter', 'XML']
+android_skills = ['Android', 'Kotlin', 'Java', 'Flutter', 'XML',
+                  'Android Development', 'Kivy', 'GIT', 'SDK', 'SQLite',
+                  'Android Studio', 'Jetpack Compose', 'Firebase', 'Material Design', 'MVVM Architecture']
 
 # IOS development skils
-ios_skills = ['iOS Development', 'Swift', 'Objective-C', 'Xcode', 'Cocoa Touch']
+ios_skills = ['iOS Development', 'Swift', 'Objective-C', 'Xcode', 'Cocoa Touch',
+              'Cocoa', 'SQLite', 'Plist', 'StoreKit', 'UI-Kit',
+              'AV Foundation', 'Auto-Layout', 'Core Data', 'SwiftUI', 'TestFlight']
 
 # UI/UX devlopment skills
-uiux_skills = ['UI', 'User Experience', 'Adobe XD', 'Figma', 'Prototyping']
+uiux_skills = ['UI', 'User Experience', 'Adobe XD', 'Figma', 'Prototyping',
+               'Zeplin', 'Balsamiq', 'Wireframes', 'Storyframes', 'Adobe Photoshop',
+               'Editing', 'Illustrator', 'After Effects', 'Premier Pro', 'InDesign',]
 
 # Soft Skills 
-soft_skill = ['Communication', 'Teamwork', 'Problem-Solving', 'Time Management', 'Adaptability', 'Critical Thinking', 'Creativity', 'Leadership', 'Attention to Detail', 'Conflict Resolution']
+soft_skill = ['Communication', 'Teamwork', 'Problem-Solving', 'Time Management', 'Adaptability', 
+              'Critical Thinking', 'Creativity', 'Leadership', 'Attention to Detail', 'Conflict Resolution']
